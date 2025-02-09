9eb014ceb007374f6269825588157d880b1290ab
suryanshvermaa <154125921+suryanshvermaa@users.noreply.github.com>
2025-02-09 19:34:55 +0530

    navbar and home section devided

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
-import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    {/* <div className="" >
+    <div className="" >
       <h1 className=" mt-80 text-center text-4xl text-blue-800 " >BYTEVERSE 2K25</h1>
-    </div> */}
-    <HeroSection/>
+    </div>
     </>
   );
 }
