895e68850534203346d84485225e2e03592666d2
Harshitver0511 <154688128+Harshitver0511@users.noreply.github.com>
2025-02-10 22:08:28 +0530

    done till faq

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 import { PrizesSection } from "@/components/PrizesSection";
+import { FAQ } from "@/components/FAQ";
 
 export default function Home() {
   return (
     <>
-    <div className="" >
+    <div className="bg-cover w-full bg-top" style={{ backgroundImage: "url('/background-images/heroSectionBackground.png')"} } >
       <HeroSection />
       <PrizesSection />
+      <FAQ/>
     </div>
     </>
   );
