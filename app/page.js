38db6f1b156d3863972565ec26674e24be4f8e4b
Harshitver0511 <154688128+Harshitver0511@users.noreply.github.com>
2025-02-14 00:37:21 +0530

    added dropdown in the navbar

--- app/page.js
+++ app/page.js
@@ -1,12 +1,9 @@
 
 import ContactUs from "@/components/ContactUs";
 import { FAQ } from "@/components/FAQ";
-import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Judges from "@/components/Judges";
 import { PrizesSection } from "@/components/PrizesSection";
-import Image from "next/image";
-import Sponsors from "./sponsers";
 import OrganizerCardList from "@/components/OrganizerCardList";
 import AboutUs from "@/components/AboutUs";
 
@@ -16,17 +13,16 @@ export default function Home() {
   return (
     <>
 
-    <div className="bg-cover w-full bg-top" style={{ backgroundImage: "url('/background-images/heroSectionBackground.png')" }}>
+    <div className="bg-cover w-full bg-top" >
       <HeroSection/>
       <AboutUs/>
       <Judges/>
 
       <OrganizerCardList/>
-      
+
       <PrizesSection/>
       <FAQ/>
       <ContactUs/>
-      <Footer/>
 
     </div>
     </>
