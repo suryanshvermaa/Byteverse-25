95e5b777cef48292f020fa09648a64229296ec57
Sandeep Kumar Gond <sandeepkumargond198@gmail.com>
2025-02-13 22:55:42 +0530

    added ScrollTriggeredCountUp in aboutus section

--- components/AboutUs.jsx
+++ components/AboutUs.jsx
@@ -1,13 +1,24 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import React, { useRef } from "react";
 import { Iceland } from "next/font/google";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import Image from "next/image";
+import useScrollTriggeredCountUp from "./useScrollTriggeredCountUp.jsx";
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
+
+const CountUp = (props) => {
+  const ref = useRef(null);
+  const count = useScrollTriggeredCountUp(ref, props.number);
+
+  return (
+    <p
+      ref={ref}
+      className="text-4xl md:text-3xl lg:text-5xl text-[#D1A878] font-bold my-0"
+    >
+      {count}+
+    </p>
+  );
+};
 
 const Iceland_font = Iceland({
   weight: "400",
@@ -19,17 +30,18 @@ export default function AboutUs() {
     <section className="text-white py-16 px-6 md:px-12 lg:px-24">
       <div className="relative flex justify-center items-center mb-12">
         <div className="absolute -left-12 w-1/4 md:w-2/5 h-0.5 bg-cyan-400 top-1/2 -translate-y-1/2"></div>
-        <h2 className={` ${Iceland_font.className} relative text-4xl sm:text-4xl md:text-6xl font-bold uppercase text-center px-4`}>
+        <h2
+          className={` ${Iceland_font.className} relative text-4xl sm:text-4xl md:text-6xl font-bold uppercase text-center px-4`}
+        >
           About Us
         </h2>
       </div>
 
-      {/* Stats Section */}
       <div className="flex flex-wrap justify-center gap-8 md:gap-16 mb-12">
         {[
-          { number: "6+", text: "Themes" },
-          { number: "36+", text: "Hours of Hacking" },
-          { number: "1.8k+", text: "Participants" },
+          { number: <CountUp number={6} />, text: "Themes" },
+          { number: <CountUp number={36} />, text: "Hours of Hacking" },
+          { number: <CountUp number={1800} />, text: "Participants" },
         ].map((stat, index) => (
           <div key={index} className="flex flex-col items-center">
             <div className=" rounded-full flex items-center justify-center">
@@ -41,13 +53,20 @@ export default function AboutUs() {
                 className="w-full h-full object-contain"
               />
             </div>
-            <p className={` ${Iceland_font.className}   sm:text-7xl font-bold -mt-6`}>{stat.number}</p>
-            <p className={` ${Iceland_font.className} text-2xl -mt-3 `}>{stat.text}</p>
+            <span
+              className={`${Iceland_font.className} text-2xl sm:text-3xl font-bold mt-[-4px] sm:mt-[-24px]`}
+            >
+              {stat.number}
+            </span>
+            <span
+              className={`${Iceland_font.className} text-base sm:text-2xl mt-[-2px] sm:mt-[-12px]`}
+            >
+              {stat.text}
+            </span>
           </div>
         ))}
       </div>
 
-      {/* Event Section */}
       <div className="bg-gray-900 px-10 mt-14 shadow-cyan-600 md:px-5 py-8 rounded-2xl flex flex-col md:flex-row gap-6 shadow-md max-w-5xl mx-auto">
         <div className="flex-1">
           <h3 className="text-3xl md:text-4xl font-bold mb-4">
@@ -73,9 +92,8 @@ export default function AboutUs() {
         </div>
       </div>
 
-      {/* Clubs Section */}
+
       <div className="relative mt-24 md:px-7  grid grid-cols-1 md:grid-cols-2 gap-14 items-center">
-        {/* Left Card - HackSlash */}
         <div className="bg-[#0E1A2B] p-6 rounded-2xl text-center shadow-lg relative">
           <Image
             src="/hackslashlogo.png"
@@ -105,7 +123,6 @@ export default function AboutUs() {
           </div>
         </div>
 
-        {/* Center Pearl Shape */}
         <div className="absolute bg-[url('/bgcenter.png')] bg-cover bg-center bg-no-repeat left-1/2 transform -translate-x-1/2 top-15 w-40 h-40 hidden md:flex items-center justify-center rounded-full shadow-lg z-10">
           <Image
             src="/abtus3.png"
@@ -116,7 +133,6 @@ export default function AboutUs() {
           />
         </div>
 
-        {/* Right Card - DesCo */}
         <div className="bg-[#0E1A2B]  p-8 rounded-2xl text-center shadow-lg  relative">
           <Image
             src="/descologo.png"
@@ -125,9 +141,7 @@ export default function AboutUs() {
             height={80}
             className="mx-auto mb-4"
           />
-          <h2 className="text-3xl  font-bold text-gray-300">
-            DesCo
-          </h2>
+          <h2 className="text-3xl  font-bold text-gray-300">DesCo</h2>
           <p className="text-gray-400 text-start text-lg md:text-xl mt-11  md:px-12">
             The premier Design Company at the NIT Patna where creativity meets
             innovation to shape the future.Desco is dedicated to nurturing a
